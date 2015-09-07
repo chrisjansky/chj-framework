3ef197a89bb7b56b2c0c38242110bd008f08f4cf
Christian Jánský <chrisjansky@gmail.com>
2015-09-07 10:53:13 +0200

    JS Improved targeting of chj-toggle.js

--- app/assets/js/chj-toggle.js
+++ app/assets/js/chj-toggle.js
@@ -1,6 +1,24 @@
 $("[data-toggle]")
   .on("click", function() {
-    var targetEl = $(this).attr("data-target") || $(this);
-    targetEl
-      .toggleClass($(this).attr("data-toggle"));
+    var
+      $thisEl = $(this),
+      targetAttr = $thisEl.attr("data-target"),
+      $destinationEl;
+
+    // If data-target attribute provided.
+    if (targetAttr) {
+      // If data-target found among siblings.
+      if ($thisEl.siblings(targetAttr).length > 0) {
+        $destinationEl = $thisEl.siblings(targetAttr)
+      } else {
+        // Find data-target going up the DOM.
+        $destinationEl = $thisEl.closest(targetAttr);
+      }
+    } else {
+      // If data-target missing, assume $this.
+      $destinationEl = $thisEl;
+    }
+
+    $destinationEl
+      .toggleClass($thisEl.attr("data-toggle"));
   });
