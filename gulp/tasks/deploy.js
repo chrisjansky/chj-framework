c2cd725d889282f511c56415a40f268dade63a7e
Christian Jánský <chrisjansky@gmail.com>
2015-12-14 17:53:00 +0100

    Gulp Replaced gulp-ftp with vinyl-ftp

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,17 +1,31 @@
 var
   gulp = require("gulp"),
   plugins = require("gulp-load-plugins")({
-    pattern: ["gulp-*"]
+    pattern: ["gulp-*", "vinyl-ftp"]
   }),
   config = require("../gulpconfig.json");
 
-gulp.task("deploy", function() {
-  return gulp.src(config.dist.root + "**/*", {base: config.dist.root})
-    .pipe(plugins.ftp({
-      host: config.ftp.host,
-      user: config.ftp.user,
-      pass: config.ftp.pass,
-      remotePath: config.ftp.path
-    }))
-    .pipe(plugins.size());
+var
+  ftpConn = plugins.vinylFtp.create({
+    host: config.ftp.host,
+    user: config.ftp.user,
+    pass: config.ftp.pass
+  });
+
+gulp.task("deploy:clean", function(callback) {
+  ftpConn.rmdir(config.ftp.path, function(error) {
+    if (error) return callback(error);
+
+    gulp.src(config.dist.root + "**/*", {base: config.dist.root, buffer: false})
+      .pipe(ftpConn.dest(config.ftp.path));
+  });
 });
+
+gulp.task("deploy:update", function() {
+  return gulp.src(config.dist.root + "**/*", {base: config.dist.root, buffer: false})
+    .pipe(ftpConn.newer(config.ftp.path))
+    .pipe(ftpConn.dest(config.ftp.path));
+});
+
+// Remove previous build by default.
+gulp.task("deploy", ["deploy:clean"]);
