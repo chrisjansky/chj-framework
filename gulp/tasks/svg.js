a272897ff7211786b07c0f788ffcfd56313e0274
Christian Jánský <chrisjansky@gmail.com>
2015-12-14 19:12:18 +0100

    Gulp Started using gulp-svg-sprite, uninstalled gulp-svg2png

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -1,27 +1,28 @@
 var
   gulp = require("gulp"),
   plugins = require("gulp-load-plugins")({
-    pattern: ["gulp-*"]
+    pattern: ["gulp-*", "del", "vinyl-paths"]
   }),
   config = require("../gulpconfig.json");
 
 // Delete the PNG fallbacks/ folder.
 gulp.task("svg:wipe", function() {
-  return gulp.src([config.dev.fallbacks, config.dev.svgBuildGlob], {read: false})
-    .pipe(vinyl(del));
+  return gulp.src(config.dev.svgBuildGlob, {read: false})
+    .pipe(plugins.vinylPaths(plugins.del));
 });
 
 // Optimize SVG.
-gulp.task("svg:optimize", ["svg:wipe"], function() {
+gulp.task("svg:sprites", ["svg:wipe"], function() {
   return gulp.src(config.dev.svgSourceGlob)
-    .pipe(plugins.imagemin())
+    .pipe(plugins.svgSprite({
+      mode: {
+        symbol: {
+          dest: ".",
+          sprite: "symbols.svg",
+        }
+      }
+    }))
     .pipe(gulp.dest(config.dev.svgBuild));
 });
 
-// Render PNG fallbacks for SVG.
-gulp.task("svg", ["svg:optimize"], function() {
-  // WTF error, needs src with ".svg".
-  return gulp.src(config.dev.svgBuildGlob + ".svg")
-    .pipe(plugins.svg2png())
-    .pipe(gulp.dest(config.dev.fallbacks));
-});
+gulp.task("svg", ["svg:sprites"]);
