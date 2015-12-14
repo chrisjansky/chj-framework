98916c3701c391bf88a6b146b132de7097d8e356
Christian Jánský <chrisjansky@gmail.com>
2015-12-14 14:58:08 +0100

    Major rewrite of gulp:guide and gulp:build tasks, uninstalled yargs, gulp-uncss

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,7 +1,6 @@
 var
   gulp = require("gulp"),
   fs = require("fs"),
-  argv = require("yargs").argv,
   plugins = require("gulp-load-plugins")({
     pattern: ["gulp-*", "del", "vinyl-paths"]
   }),
@@ -9,10 +8,8 @@ var
 
 // Delete the previous build.
 gulp.task("build:wipe", function() {
-  if (argv.full) {
-    return gulp.src(config.dist.root, {read: false})
-      .pipe(plugins.vinylPaths(plugins.del));
-  } else return;
+  return gulp.src(config.dist.root, {read: false})
+    .pipe(plugins.vinylPaths(plugins.del));
 });
 
 // Read paths to assets.
@@ -30,20 +27,24 @@ gulp.task("build:move", ["build:wipe", "build:resources"], function() {
 
 // Minify CSS and JS.
 gulp.task("build:compile", ["build:move"], function() {
-  return gulp.src(config.dev.styleguide + "index.html")
+  return gulp.src([config.dev.root + "index.html", config.dev.styleguide + "index.html"], {base: config.dev.root})
     .pipe(plugins.usemin(
       {
-        js: [plugins.uglify()],
-        css: [
-          plugins.autoprefixer({
+        enableHtmlComment: false,
+        js: [function () {
+          return plugins.uglify()
+        }],
+        css: [function () {
+          return plugins.autoprefixer({
             cascade: false
           }),
           plugins.minifyCss({
             keepSpecialComments: 0
-        })]
+          })
+        }]
       }
     ))
-    .pipe(gulp.dest(config.dist.styleguide))
+    .pipe(gulp.dest(config.dist.root))
     .pipe(plugins.size({
       showFiles: true
     }));
@@ -53,37 +54,35 @@ gulp.task("build:compile", ["build:move"], function() {
 gulp.task("build:inject", ["build:compile"], function() {
   return gulp.src(config.dist.root + "**/*.html")
     .pipe(plugins.inject(gulp.src(
-        resources.injectDist.site, {read: false}), {
-          relative: true,
-          name: "site"
-        }
-      ))
-      .pipe(plugins.inject(gulp.src(
-        resources.injectDist.vendor, {read: false}), {
-          relative: true,
-          name: "vendor"
-        }
-      ))
-      .pipe(plugins.inject(gulp.src(
-        resources.injectDist.guide, {read: false}), {
-          relative: true,
-          name: "guide"
-        }
-      ))
+      resources.injectDist.site, {read: false}), {
+        relative: true,
+        name: "site"
+      }
+    ))
+    .pipe(plugins.inject(gulp.src(
+      resources.injectDist.vendor, {read: false}), {
+        relative: true,
+        name: "vendor"
+      }
+    ))
+    .pipe(plugins.inject(gulp.src(
+      resources.injectDist.guide, {read: false}), {
+        relative: true,
+        name: "guide"
+      }
+    ))
     .pipe(gulp.dest(config.dist.root));
 });
 
-// Minify images if provided with --full argument.
+// Minify images.
 gulp.task("build:images", ["build:wipe"], function() {
-  if (argv.full) {
-    return gulp.src([config.dev.imagesGlob, config.dev.imagesIgnore], {base: config.dev.root})
-      .pipe(plugins.imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}]
-      }))
-      .pipe(gulp.dest(config.dist.root));
-  }
+  return gulp.src([config.dev.imagesGlob, config.dev.imagesIgnore], {base: config.dev.root})
+    .pipe(plugins.imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}]
+    }))
+    .pipe(gulp.dest(config.dist.root));
 });
 
-// Wipe first. Move, produce. Images if --full.
+// Wipe first. Move, produce. Images in parallel with it.
 gulp.task("build", ["build:inject", "build:images"]);
